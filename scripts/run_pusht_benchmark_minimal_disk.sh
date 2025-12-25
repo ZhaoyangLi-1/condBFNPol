e7af40250b6acff7a4b1e0418084c5ec9142f895
aleynakara <ge87gob@mytum.de>
2025-12-25 01:14:00 +0100

    update the benchmark

--- scripts/run_pusht_benchmark_minimal_disk.sh
+++ scripts/run_pusht_benchmark_minimal_disk.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=pusht_benchmark
-#SBATCH --partition=lrz-hgx-h100-94x4,lrz-hgx-a100-80x4,lrz-dgx-a100-80x8,mcml-hgx-h100-94x4,mcml-hgx-a100-80x4
-#SBATCH --qos=gpu
+#SBATCH --partition=lrz-hgx-h100-94x4,lrz-hgx-a100-80x4,lrz-dgx-a100-80x8
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
